7666d9988d1fea09fcfec37aae174c4c560573fa
Clark Sell <clark.sell@csell.net>
2018-08-02 09:09:23 -0500

    bug fixes on empty documents

--- src/api/room/screen.js
+++ src/api/room/screen.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import logger from '../../utilities/logger';
 
 // todo: make a argument somewhere.
@@ -53,43 +54,48 @@ const updateSession = (sessionId, session, db) => {
 };
 
 const post = async (request, response) => {
-  logger.trace('session update called');
-  logger.data(request.body);
+  try {
+    logger.trace('session update called');
+    logger.data(request.body);
 
-  const pubsub = request.app.get('pubsub');
-  const db = request.app.get('db');
-  const newSession = request.body;
+    const pubsub = request.app.get('pubsub');
+    const db = request.app.get('db');
+    const newSession = request.body;
 
-  // get the current session ( if exists ) as we need to know what room will be effected.
-  logger.debug('getting original session information');
-  const originalSession = await getSessionsById(newSession.id, db);
+    // get the current session ( if exists ) as we need to know what room will be effected.
+    logger.debug('getting original session information');
+    const originalSession = await getSessionsById(newSession.id, db);
 
-  // update the new session the session
-  logger.debug('updating the db with the new session information');
-  await updateSession(newSession.id, newSession, db);
+    // update the new session the session
+    logger.debug('updating the db with the new session information');
+    await updateSession(newSession.id, newSession, db);
 
-  // get all sessions for affected room
-  logger.debug('Getting session list for affected rooms');
-  const sessionList = await getSessionsByRoom(tcEventId, newSession.scheduledRoom, db);
+    // get all sessions for affected room
+    logger.debug('Getting session list for affected rooms');
+    const sessionList = await getSessionsByRoom(tcEventId, newSession.scheduledRoom, db);
 
-  // if the room changed, get old room list and update clients
-  if (originalSession) {
-    if (originalSession.scheduledRoom.toLowerCase() !== newSession.scheduledRoom.toLowerCase()) {
-      const oldRoomSessionList = await getSessionsByRoom(
-        tcEventId,
-        originalSession.scheduledRoom,
-        db,
-      );
-      logger.debug('publishing new info to old room list');
-      pubsub.publish('roomScreenChanged', oldRoomSessionList);
+    // if the room changed, get old room list and update clients
+    if (!_.isEmpty(originalSession)) {
+      if (originalSession.scheduledRoom.toLowerCase() !== newSession.scheduledRoom.toLowerCase()) {
+        const oldRoomSessionList = await getSessionsByRoom(
+          tcEventId,
+          originalSession.scheduledRoom,
+          db,
+        );
+        logger.debug('publishing new info to old room list');
+        pubsub.publish('roomScreenChanged', oldRoomSessionList);
+      }
     }
+
+    // update all clients new room
+    logger.debug('publishing new info to new room list');
+    pubsub.publish('roomScreenChanged', sessionList);
+
+    response.sendStatus(200);
+  } catch (e) {
+    logger.error(e);
+    response.send(500);
   }
-
-  // update all clients new room
-  logger.debug('publishing new info to new room list');
-  pubsub.publish('roomScreenChanged', sessionList);
-
-  response.sendStatus(200);
 };
 
 // eslint-disable-next-line
