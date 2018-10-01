c637e6520cdf068f8dc12c0eaeeda47207af1600
Clark Sell <clark.sell@csell.net>
2018-10-01 14:20:21 -0500

    working on payload on health data

--- src/api/brb/health.js
+++ src/api/brb/health.js
@@ -1,14 +1,54 @@
 import logger from '../../utilities/logger';
 
-/*
+/* Example Playload
 
-sample payload
+{
+  device: {
+    system: {
+      uptime: 334961,
+      memory: {
+        total: 83200,
+        used: 32496,
+      },
+    },
+    network: {
+      connection: {
+        status: 4,
+        error: 1024,
+        disconnects: 0,
+        attempts: 9,
+        disconnect: 0,
+      },
+      signal: {
+        rssi: -50,
+        strength: 100,
+        quality: 100,
+        qualityv: 42,
+        at: 1,
+        strengthv: -50,
+      },
+    },
+    cloud: {
+      connection: {
+        status: 2, error: 0, attempts: 1, disconnect: 0,
+      },
+      disconnects: 0,
+      publish: { rate_limited: 0 },
+      coap: { unack: 0 },
+    },
+  },
+  service: {
+    device: { status: 'ok' },
+    coap: { round_trip: 70 },
+    cloud: { uptime: 334613, publish: { sent: 61347 } },
+  },
+};
 
 */
 
 const post = (request, response) => {
   logger.trace('particle device health updated');
-  logger.data('device device health data:', request.body.data);
+  logger.data('device health data:', request.body);
 
   const newPayload = request.body;
   newPayload.data = JSON.parse(request.body.data);
